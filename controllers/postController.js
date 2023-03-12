565016e390579c7acdee8bc43991823d98324e3e
Kyrylo Hliebov <121131208+xenoniiiii@users.noreply.github.com>
2023-03-12 15:26:47 +0200

    bug fixing

--- controllers/postController.js
+++ controllers/postController.js
@@ -55,26 +55,21 @@ export const deletePost = async (req, res) => {
     try {
         const postId = req.params.id;
 
-        await postModel.findOneAndDelete({
-            _id: postId
-
-        }, (err, doc) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    message: "Failed to delete article"
-                });
-            };
-
+        await postModel.findOneAndDelete(
+            {
+                _id: postId
+            },
+            
+        ).then (doc => {
             if (!doc) {
                 return res.status(404).json({
-                    message: "Article not found"
+                    message: "Article is not found"
                 });
-            };
-
-            res.json({
-                success: true
-            });
+            }
+        })
+        
+        res.json({
+            success: true
         });
 
     } catch (err) {
